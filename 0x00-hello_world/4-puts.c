da8ee7124ee7a760e7427d9e29acee9f2fd99c60
habtszz <habtszz@gmail.com>
2022-03-10 13:18:34 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h
+#include<stdio.h>
 
 /**
- * main - prints the phrase "programming is like building a multilingual puzzle"
+ * main - prints the phrase "programming is like building a multilingual puzzle
  * Return: 0 if excited properly, non-zero otherwise
  */
 int main(void)
     {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+             puts("\"programming is like building a multilingual puzzle");
+             return (0);
     }
 
