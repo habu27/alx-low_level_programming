4654f44090c3beaeb33d9b3ecd0a7b6f9125f9f1
habtszz <habtszz@gmail.com>
2022-03-12 02:57:03 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 char lower_ch;
 char upper_ch;
 
-for (lower_ch = 'a': lower_ch <= 'z': ch++)
+for (lower_ch = 'a'; lower_ch <= 'z'; ch++)
 {
 putchar(lower_ch);
 }
 
-for (upper_ch = 'A': upper_ch <= 'z': ch++)
+for (upper_ch = 'A'; upper_ch <= 'z'; ch++)
 {
 putchar(upper_ch);
 }
