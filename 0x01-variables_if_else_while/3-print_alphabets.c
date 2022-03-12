aaaff2fc1c051ef1d91edbc8c69e584fdfdfba9d
habtszz <habtszz@gmail.com>
2022-03-12 02:37:56 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z' ch++)
+{
+putchar(ch);
+}
+
+for (ch = 'A'; ch <= 'z' ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+
+}
