eb7c2bbebc38adefa4619c05a62c99c9b6205e67
habtszz <habtszz@gmail.com>
2022-03-12 02:23:56 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+char ch;
+
+for (ch = 'a': ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+}
