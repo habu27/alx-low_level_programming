b2a8bb903f56b537fb91fa43c958b37f6fbda7da
habtszz <habtszz@gmail.com>
2022-03-13 04:29:13 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alwayss 0 (success)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; n < 58; i++)
+{
+putchar(i);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+
+putchar('\n');
+return (0);
+}
