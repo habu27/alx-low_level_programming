7a1f06d2f5d95ab5d54a3c1eff4aa5807eaf0c4c
habtszz <habtszz@gmail.com>
2022-03-13 01:49:33 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always (success)
+ */
+
+int main(void)
+
+{
+char  ch;
+for (ch 'a'; ch <= 'z'; ch++)
+{
+if (ch == 'q')
+continue:
+
+else if(ch == 'e')
+continue:
+
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+
+}
