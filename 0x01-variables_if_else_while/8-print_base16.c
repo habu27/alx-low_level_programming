eb017cd3bf10ebd6ae4dd8b58c9af643384336e2
habtszz <habtszz@gmail.com>
2022-03-13 04:12:35 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+ 
+{
+int i;
+char ch;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
