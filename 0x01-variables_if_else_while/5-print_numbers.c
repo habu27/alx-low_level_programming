75216027e84ebf90ecf2ef0377153cc8825edb5e
habtszz <habtszz@gmail.com>
2022-03-13 03:06:15 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+
+{
+int h;
+
+for (h = 0 ; h < 10; h++)
+
+{
+printf("%dh", h);
+}
+
+printf('\n');
+
+return (0);
+
+}
