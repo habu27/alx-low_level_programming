760a71fcc8dabbe7c3f9a94a16f7634f59a95ba3
habu27 <100352651+habu27@users.noreply.github.com>
2022-03-15 03:29:03 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,11 @@
 
 /**
  * main - entry point
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -19,4 +17,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-return (0); }
+return (0);
+}
