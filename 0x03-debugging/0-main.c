cdb1938530e7b4aef398821c3e38dbc2df958be0
habu27 <100352651+habu27@users.noreply.github.com>
2022-03-16 04:03:19 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 
 /**
@@ -9,14 +7,10 @@
 
 int main(void)
 {
-       int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        i = 0;
+        positive_or_negative(i);
 
-	if (n == 0)
-{
-	printf("%d is zero\n", n);
-}
-	return (0);
+        return (0);
 }
